b917536c9a8a9b757c8da4352c14bfce54077ccc
xyclose11 <harrisgus1@hotmail.com>
2024-06-26 15:36:55 -0500

    Updated styling

--- wwwroot/js/Views/Note/noteViewChange.js
+++ wwwroot/js/Views/Note/noteViewChange.js
@@ -1,17 +1,12 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const jquery_1 = __importDefault(require("jquery"));
-document.addEventListener('DOMContentLoaded', () => {
+import $ from 'jquery';
+$(document).ready(function () {
     // TableView Pagination Handlers
-    (0, jquery_1.default)('body').on('click', '.pagination-link', function (e) {
+    $('body').on('click', '.pagination-link', function (e) {
         e.preventDefault();
-        const url = (0, jquery_1.default)(this).attr('href');
+        const url = $(this).attr('href');
         if (url) {
-            jquery_1.default.get(url, function (data) {
-                (0, jquery_1.default)('#noteViewTableContainer').html(data);
+            $.get(url, function (data) {
+                $('#noteViewTableContainer').html(data);
             });
         }
     });
@@ -22,26 +17,18 @@ document.addEventListener('DOMContentLoaded', () => {
     let storedTagIds = JSON.parse(localStorage.getItem('selectedTagIds') || '[]');
     // Repopulates view selector with users choice
     if (storedView) {
-        (0, jquery_1.default)('#viewSelector').val(storedView);
+        $('#viewSelector').val(storedView);
         loadView(storedView, storedCategory, storedTagIds);
     }
-    (0, jquery_1.default)(document).ready(function () {
-        if (storedTagIds) {
-            storedTagIds.map(function (tag) {
-                console.log("tagFCheckbox+" + tag);
-                (0, jquery_1.default)(`input[type="checkbox"][id="tagFCheckbox+${tag}"]`).addClass('ThisISAteest');
-            });
-        }
-    });
-    (0, jquery_1.default)('#viewSelector').change(function () {
-        const selectedView = (0, jquery_1.default)(this).val();
+    $('#viewSelector').change(function () {
+        const selectedView = $(this).val();
         localStorage.setItem('noteView', selectedView);
         loadView(selectedView, storedCategory, storedTagIds);
     });
     // End NoteView Handlers
 });
 function loadView(view, category, selectedTagIds) {
-    jquery_1.default.ajax({
+    $.ajax({
         url: '/Note/GetNotes/',
         type: 'GET',
         data: {
@@ -50,7 +37,7 @@ function loadView(view, category, selectedTagIds) {
             filterTags: selectedTagIds.join(','),
         },
         success: function (data) {
-            (0, jquery_1.default)('#notesSection').html(data);
+            $('#notesSection').html(data);
         }
     });
 }
